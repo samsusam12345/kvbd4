a4ef50d72e1b2f37b1a52a517c7f9e4764e641c7
samsusam12345 <70570382+samsusam12345@users.noreply.github.com>
2020-12-05 20:32:26 +0700

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using CryptoUtilsLib;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Net;
+
+namespace client
+{
+    class Program
+    {
+        static volatile TcpClient client = null;
+        static volatile NetworkStream stream = null;
+        static volatile string[] alluser;
+        static volatile string interlocutorV;
+        static volatile string interlocutorUserName;
+
+        static void ProoveConn(BigInteger N, BigInteger S)
+        {
+            TcpClient proover = new TcpClient("127.0.0.1", 9559);
+            NetworkStream prooverStream = proover.GetStream();
+            Console.WriteLine("Connected");
+            byte[] bytes = new byte[1024];
+            int length;
+            BigInteger R, X, Y, e;
+
+            for(int i=0; i< 15; i++)
+            {
+                R = Fiat_Shamir.get_R(N);
+                X = BigInteger.ModPow(R, 2, N);
+
+                bytes = new byte[1024];
+                bytes = Encoding.ASCII.GetBytes(X.ToString());
+                prooverStream.Write(bytes, 0, bytes.Length);
+                prooverStream.Flush();
+
+                while (!prooverStream.DataAvailable) ;
+                bytes = new byte[1024];
+                length = prooverStream.Read(bytes, 0, bytes.Length);
+                e = BigInteger.Parse(Encoding.ASCII.GetString(bytes, 0, length));
+
+                Y = (R * (BigInteger.Pow(S, (int)e))) % N;
+                bytes = new byte[1024];
+                bytes = Encoding.ASCII.GetBytes(Y.ToString());
+                prooverStream.Write(bytes, 0, bytes.Length);
+                prooverStream.Flush();
+                Console.WriteLine("Round {0} done.", i+1);
+            }
+            while (!prooverStream.DataAvailable) ;
+            byte[] response = new byte[1];
+            prooverStream.Read(response, 0, 1);
+            Console.WriteLine(response[0] == 1 ? "Authentication success" : "Authentication failed\nConnection closed");
+
+            byte[] response1 = new byte[128];
+            response1 = Encoding.ASCII.GetBytes(response[0] == 1 ? "AuthOK": "!AuthOK");
+            stream.Write(response1, 0, response1.Length);
+            stream.Flush();
+        }
+
+        static void ServerMode(BigInteger N)
+        {
+            TcpListener serverExchange = new TcpListener(IPAddress.Parse("127.0.0.1"), 9559);
+            serverExchange.Start();
+            Console.WriteLine(interlocutorUserName + "'s V = " + interlocutorV);
+            Console.WriteLine("Wait connection...");
+            TcpClient verifier = serverExchange.AcceptTcpClient();
+            NetworkStream verifierStream = verifier.GetStream();
+            Console.WriteLine("Connected");
+
+            int success = 0;
+            byte[] bytes = new byte[1024];
+            int length;
+            BigInteger e, X, Y, V = BigInteger.Parse(interlocutorV);
+
+            for (int i = 0; i < 15; i++)
+            {
+                while (!verifierStream.DataAvailable) ;
+                bytes = new byte[1024];
+                length = verifierStream.Read(bytes, 0, bytes.Length);
+                X = BigInteger.Parse(Encoding.ASCII.GetString(bytes, 0, length));
+
+                e = Fiat_Shamir.get_e();
+                bytes = new byte[1024];
+                bytes = Encoding.ASCII.GetBytes(e.ToString());
+                verifierStream.Write(bytes, 0, bytes.Length);
+                verifierStream.Flush();
+
+                while (!verifierStream.DataAvailable) ;
+                bytes = new byte[1024];
+                length = verifierStream.Read(bytes, 0, bytes.Length);
+                Y = BigInteger.Parse(Encoding.ASCII.GetString(bytes, 0, length));
+
+                if (BigInteger.ModPow(Y, 2, N) == (X * BigInteger.Pow(V, (int)e)) % N) {
+                    success++;
+                    Console.WriteLine("Round {0} successful!", i+1);
+                }
+                else
+                    Console.WriteLine("Round {0} not successful!", i + 1);
+            }
+            byte s = 0;
+            if(success == 15) {
+                Console.WriteLine("100% succes");
+                s = 1;
+            }
+            else
+                Console.WriteLine("Authentication failed\nConnection closed");
+            verifierStream.Write(new byte[] { s }, 0, 1);
+            verifierStream.Flush();
+        }
+
+        static void ClientInterface(BigInteger N)
+        {
+            while (true) {
+                byte[] bytes = new byte[256];
+                Console.WriteLine("Enter command:");
+                string cmd = Console.ReadLine();
+                if (cmd.ToLower() == "auth")
+                {
+                    bytes = Encoding.ASCII.GetBytes(cmd.ToLower());
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Flush();
+
+                    int number = int.Parse(Console.ReadLine()) - 1;
+                    string SelectedUser = "StartWith$" + alluser[number];
+                    Console.WriteLine("You select " + alluser[number]);
+                    bytes = new byte[256];
+                    bytes = Encoding.ASCII.GetBytes(SelectedUser);
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Flush();
+                }
+                else if (cmd.ToLower() == "y" || cmd.ToLower() == "n")
+                {
+                    if (cmd.ToLower() == "y")
+                    {
+                        bytes = new byte[256];
+                        bytes = Encoding.ASCII.GetBytes("AnswerYes$" + interlocutorUserName);
+                        stream.Write(bytes, 0, bytes.Length);
+                        stream.Flush();
+                        Task ServerOprion = Task.Run(() => ServerMode(N));
+                    }
+                    else
+                    {
+                        bytes = new byte[256];
+                        bytes = Encoding.ASCII.GetBytes("AnswerNo$"+interlocutorUserName);
+                        stream.Write(bytes, 0, bytes.Length);
+                        stream.Flush();
+                    }
+                }
+                else
+                    Console.WriteLine("Unknown command");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[1024];
+            Console.Write("Enter your Name: ");
+            string name = Console.ReadLine();
+            client = new TcpClient("127.0.0.1", 9478);
+            stream = client.GetStream();
+
+            int length = stream.Read(bytes, 0 , bytes.Length); 
+            BigInteger N = BigInteger.Parse(Encoding.ASCII.GetString(bytes, 0, length));
+            BigInteger S = Fiat_Shamir.get_S(N);
+            BigInteger V = BigInteger.ModPow(S, 2, N);
+
+            bytes = Encoding.ASCII.GetBytes(name+"$"+V.ToString());
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Flush();
+
+            bytes = new byte[512];
+            while (!stream.CanRead);
+            length = stream.Read(bytes, 0, bytes.Length);
+            Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, length));
+            Console.WriteLine("N = "+N);
+            Console.WriteLine("S = "+S);
+            Console.WriteLine("V = "+V);
+
+            Task Exchange = Task.Run(() => ClientInterface(N));
+
+            while (true)
+            {
+                while (!stream.CanRead) ;
+                bytes = new byte[512];
+                length = stream.Read(bytes, 0, bytes.Length);
+                string response = Encoding.ASCII.GetString(bytes, 0, length);
+
+                if (response.Contains("has connected"))
+                    Console.WriteLine(response);
+                else if(response.Contains("New Request"))
+                {
+                    string[] newrequest = response.Split('$');
+                    interlocutorUserName = newrequest[1];
+                    interlocutorV = newrequest[2];
+                    Console.WriteLine("You have "+newrequest[0].ToLower()+" "+ interlocutorUserName);
+                    Console.WriteLine("Accept request [y/n]?");
+                }
+                else if (response.Contains("deny"))
+                {
+                    Console.WriteLine(response);
+                }
+                else if(response == "ready")
+                {
+                    Console.WriteLine("Trying connect...");
+                    Task newConn = Task.Run(() => ProoveConn(N, S));
+                }
+                else //there are all users
+                {
+                    alluser = response.Split('$');
+                    Console.WriteLine("Select user");
+                    int i = 1;
+                    foreach (var user in alluser) {
+                        if(user.ToString() != string.Empty)
+                            Console.WriteLine(i++ + ") " + user);
+                    }
+                }
+            }
+            Console.ReadKey();
+            client.Close();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+using CryptoUtilsLib;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Connections
+    {
+        public TcpClient tcpClient { get; set; }
+        public NetworkStream clientNetwork { get; set; }
+        public string name { get; set; }
+        public BigInteger V { get; set; }
+    }
+
+    class Program
+    {
+        static List<Connections> Clients = new List<Connections> { };
+        static volatile string currentName = null;
+        static volatile string NamefromThread = null;
+        static volatile string VfromThread = null;
+        static volatile string command = null;
+
+        static void Linker(Connections client)
+        {
+            byte[] bytes = new byte[1024];
+            while (true) {
+                if (client.name == NamefromThread) {
+                    if (command == "start")
+                    {
+                        bytes = Encoding.ASCII.GetBytes("New Request from$" + currentName + "$" + VfromThread);
+                        client.clientNetwork.Write(bytes, 0, bytes.Length);
+                        client.clientNetwork.Flush();
+                        VfromThread = string.Empty;
+                        NamefromThread = string.Empty;
+                        command = string.Empty;
+                        currentName = string.Empty;
+                    }
+                    else if (command == "No")
+                    {
+                        bytes = new byte[512];
+                        bytes = Encoding.ASCII.GetBytes(currentName + " deny your request");
+                        client.clientNetwork.Write(bytes, 0, bytes.Length);
+                        client.clientNetwork.Flush();
+                        NamefromThread = string.Empty;
+                        command = string.Empty;
+                        currentName = string.Empty;
+                    }
+                    else if(command == "Yes")
+                    {
+                        bytes = new byte[256];
+                        bytes = Encoding.ASCII.GetBytes("ready");
+                        client.clientNetwork.Write(bytes, 0, bytes.Length);
+                        client.clientNetwork.Flush();
+                        NamefromThread = string.Empty;
+                        command = string.Empty;
+                        currentName = string.Empty;
+                    }
+                }
+            }
+        }
+
+        static void ResponseProccessing(Connections client)
+        {
+            Task holder = Task.Run(() => Linker(client));
+            string selectedName = null; ;
+            while (true)
+            {
+                if (client.clientNetwork.DataAvailable)
+                {
+                    byte[] bytes = new byte[256];
+                    int length = client.clientNetwork.Read(bytes, 0, bytes.Length);
+                    string response = Encoding.ASCII.GetString(bytes, 0, length);
+                    if (response == "auth")
+                    {
+                        Console.WriteLine("[{0}] get auth from " + client.name, DateTime.Now.ToString());
+                        string allusers = string.Empty;
+                        foreach (var user in Clients)
+                        {
+                            if (user.name == client.name)
+                                continue;
+                            else
+                                allusers += user.name + "$";
+                        }
+                        bytes = Encoding.ASCII.GetBytes(allusers);
+                        client.clientNetwork.Write(bytes, 0, bytes.Length);
+                        client.clientNetwork.Flush();
+                    }
+                    else if (response.Contains("StartWith"))
+                    {
+                        selectedName = response.Split('$')[1];
+                        Console.WriteLine("[{0}] "+client.name + " selected " + selectedName, DateTime.Now.ToString());
+                        command = "start";
+                        currentName = client.name;
+                        VfromThread = client.V.ToString();
+                        NamefromThread = selectedName;
+                    }
+                    else if (response.Contains("AnswerNo"))
+                    {
+                        selectedName = response.Split('$')[1];
+                        Console.WriteLine("[{0}] "+client.name + " deny request from " + selectedName, DateTime.Now.ToString());
+                        command = "No";
+                        currentName = client.name;
+                        NamefromThread = selectedName;
+                    }
+                    else if (response.Contains("AnswerYes"))
+                    {
+                        selectedName = response.Split('$')[1];
+                        Console.WriteLine("[{0}] "+client.name + " accept request from " + selectedName, DateTime.Now.ToString());
+                        command = "Yes";
+                        currentName = client.name;
+                        NamefromThread = selectedName;
+                    }
+                    else if(response == "AuthOK")
+                    {
+                        Console.WriteLine("[{0}] "+client.name+" authentiacated", DateTime.Now.ToString());
+                    }
+                    else if (response == "!AuthOK")
+                    {
+                        Console.WriteLine("[{0}] " + client.name + " not authentiacated", DateTime.Now.ToString());
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TcpClient newclient = null;
+            NetworkStream stream = null;
+            string ClientName = string.Empty;
+            byte[] bytes = new byte[1024];
+
+            (BigInteger, BigInteger) PQ = Fiat_Shamir.get_PQ();
+            BigInteger N = PQ.Item1 * PQ.Item2;
+
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9478);
+            server.Start();
+            Console.WriteLine("Server start...");
+            Console.WriteLine("N = " + N);
+
+            while (true)
+            {
+                newclient = server.AcceptTcpClient();
+                stream = newclient.GetStream();
+                bytes = Encoding.ASCII.GetBytes(N.ToString());
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Flush();
+
+                bytes = new byte[1024];
+                while (!stream.CanRead) ;
+                int length = stream.Read(bytes, 0, bytes.Length);
+                string[] data = Encoding.ASCII.GetString(bytes, 0, length).Split('$');
+                ClientName = data[0];
+                BigInteger _V = BigInteger.Parse(data[1]);
+                Console.WriteLine("[{0}] Client connected", DateTime.Now.ToString());
+
+                Connections Currentclient = new Connections { tcpClient = newclient, clientNetwork = stream, name = ClientName, V = _V };
+
+                bytes = new byte[1024];
+                bytes = Encoding.ASCII.GetBytes("Welcome");
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Flush();
+
+                foreach (var client in Clients)
+                {
+                    stream = client.tcpClient.GetStream();
+                    bytes = Encoding.ASCII.GetBytes(ClientName + " has connected");
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Flush();
+                }
+                Clients.Add(new Connections { tcpClient = newclient, clientNetwork = stream, name = ClientName, V = _V });
+                Console.WriteLine("[{0}] count of clients = " + Clients.Count, DateTime.Now.ToString());
+                int i = 1;
+                foreach (var clent in Clients)
+                    Console.WriteLine(i++ + ") " + clent.name);
+
+                Task th = Task.Run(() => ResponseProccessing(Currentclient));
+            }
+            server.Stop();
+        }
+    }
+}
